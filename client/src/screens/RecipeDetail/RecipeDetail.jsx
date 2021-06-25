78fddfe0d9b8ec576a8fd76b2ea61687b7f16dad
Carri-anne Hamer <chamer79@gmail.com>
2021-06-25 16:23:06 -0400

    netlify debugging

--- client/src/screens/RecipeDetail/RecipeDetail.jsx
+++ client/src/screens/RecipeDetail/RecipeDetail.jsx
@@ -1,20 +1,20 @@
-import "./RecipeDetail.css";
-import { useState } from "react";
-import { useParams } from "react-router-dom";
-import { getOneRecipe } from "../services/foods";
+// import "./RecipeDetail.css";
+// import { useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { getOneRecipe } from "../services/foods";
 
 
-export default function RecipeDetail(props) {
-  const [recipeItem, setRecipeItem] = useState(null);
-  const [recipeId, setRecipeId] = useState("");
-  const { id } = useParams();
+// export default function RecipeDetail(props) {
+//   const [recipeItem, setRecipeItem] = useState(null);
+//   const [recipeId, setRecipeId] = useState("");
+//   const { id } = useParams();
 
 
   
-  return (
-    <div>
-      
-      <h3>Recipe Detail Page</h3>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <h1>HI</h1>
+//       <h3>Recipe Detail Page</h3>
+//     </div>
+//   )
+// }

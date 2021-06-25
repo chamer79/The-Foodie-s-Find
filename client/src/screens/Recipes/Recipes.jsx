b33ec78b177090de2bb8f17b436a7b9f4448c0bb
Carri-anne Hamer <chamer79@gmail.com>
2021-06-25 13:25:00 -0400

    cleaned recipes.jsx code

--- client/src/screens/Recipes/Recipes.jsx
+++ client/src/screens/Recipes/Recipes.jsx
@@ -1,18 +1,10 @@
-import "./Recipes.css"
-import { Link } from "react-router-dom";
+import "./Recipes.css";
 import RecipeCards from "../../components/RecipeCards/RecipeCards.jsx";
 
-
-export default function Recipes({recipes}) {
+export default function Recipes({ recipes }) {
   return (
     <div className="recipes">
-   
-      <RecipeCards recipes={recipes} />  
-      
-        <Link to="recipes/1">
-          <h3>Recipes Page</h3>
-        </Link>
-     
+      <RecipeCards recipes={recipes} />
     </div>
-  )
+  );
 }

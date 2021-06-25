dda13fa4db9736ce967af66a14689150d71e47e9
Carri-anne Hamer <chamer79@gmail.com>
2021-06-25 13:58:33 -0400

    push before deployment lesson

--- client/src/screens/RecipeDetail/RecipeDetail.jsx
+++ client/src/screens/RecipeDetail/RecipeDetail.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import "./RecipeDetail.css";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { getOneRecipe } from "../services/foods";
 
-export default function RecipeDetail() {
+
+export default function RecipeDetail(props) {
+  const [recipeItem, setRecipeItem] = useState(null);
+  const [recipeId, setRecipeId] = useState("");
+  const { id } = useParams();
+
+
+  
   return (
     <div>
+      
       <h3>Recipe Detail Page</h3>
     </div>
   )

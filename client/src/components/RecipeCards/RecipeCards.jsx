60c9d579fd5692e1eb70015a7d5c592bb903c1d8
Carri-anne Hamer <chamer79@gmail.com>
2021-06-25 22:12:17 -0400

    partial recipedetail.jsx code & modified recipes.jsx

--- client/src/components/RecipeCards/RecipeCards.jsx
+++ client/src/components/RecipeCards/RecipeCards.jsx
@@ -10,12 +10,12 @@ export default function RecipeCards(props) {
       key={recipe?.id}
       id={recipe?.id}
       name={recipe?.name}
-      prep_time={recipe?.prep_time}
-      baking_cooking_time={recipe?.baking_cooking_time}
-      servings={recipe?.servings}
-      ingredients={recipe?.ingredients}
-      directions={recipe?.directions}
       img_url={recipe?.img_url}
+      // prep_time={recipe?.prep_time}
+      // baking_cooking_time={recipe?.baking_cooking_time}
+      // servings={recipe?.servings}
+      // ingredients={recipe?.ingredients}
+      // directions={recipe?.directions}
     />
   ));
 
--- client/src/screens/RecipeDetail/RecipeDetail.jsx
+++ client/src/screens/RecipeDetail/RecipeDetail.jsx
@@ -1,5 +1,5 @@
 // import "./RecipeDetail.css";
-// import { useState } from "react";
+// import { useEffect, useState } from "react";
 // import { useParams } from "react-router-dom";
 // import { getOneRecipe } from "../services/foods";
 
@@ -7,11 +7,23 @@
 //   const [recipeItem, setRecipeItem] = useState(null);
 //   const [recipeId, setRecipeId] = useState("");
 //   const { id } = useParams();
+  
+//   useEffect(() => {
+//     const fetchRecipeItem = async () => {
+//       const recipeData = await getOneFood(id);
+//       setRecipeItem(recipeData);
+//     };
+//     fetchRecipeItem();
+//   }, [id]);
 
 //   return (
-//     <div>
-
+//     <div className="receipe-card-container">
 //       <h3>Recipe Detail Page</h3>
+  
+//       <div className="img-container">
+//         <img className="recipe-img" scr={} />
+//       </div>
+
 //     </div>
 //   )
 // }

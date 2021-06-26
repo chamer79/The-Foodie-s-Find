db4c6aa459c8284688d20458c1245fc05aca2b8a
Carri-anne Hamer <chamer79@gmail.com>
2021-06-25 20:34:10 -0400

    push/pull

--- client/src/screens/RecipeDetail/RecipeDetail.jsx
+++ client/src/screens/RecipeDetail/RecipeDetail.jsx
@@ -3,17 +3,14 @@
 // import { useParams } from "react-router-dom";
 // import { getOneRecipe } from "../services/foods";
 
-
 // export default function RecipeDetail(props) {
 //   const [recipeItem, setRecipeItem] = useState(null);
 //   const [recipeId, setRecipeId] = useState("");
 //   const { id } = useParams();
 
-
-  
 //   return (
 //     <div>
-//       <h1>HI</h1>
+
 //       <h3>Recipe Detail Page</h3>
 //     </div>
 //   )

a16b88544e71abeb083e8647c31e93d1e336ad26
Carri-anne Hamer <chamer79@gmail.com>
2021-06-26 12:51:59 -0400

    data mostly rendering on recipedetail.jsx. need to fix img urls

--- client/src/screens/RecipeDetail/RecipeDetail.jsx
+++ client/src/screens/RecipeDetail/RecipeDetail.jsx
@@ -1,10 +1,27 @@
 import "./RecipeDetail.css";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useParams, useHistory, Link } from "react-router";
+import { getOneRecipe, deleteRecipe } from "../../services/recipes";
 
 export default function RecipeDetail(props) {
-  // const [recipeItem, setrecipeItem] = useState()
+  const [recipeItem, setRecipeItem] = useState(null);
+  const [isLoaded, setLoaded] = useState(false);
   const { id } = useParams();
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      const recipeItem = await getOneRecipe(id);
+      setRecipeItem(recipeItem);
+      setLoaded(true);
+    };
+    fetchRecipe();
+  }, [id]);
+
+  if (!isLoaded) {
+    return <h1>Loading...</h1>;
+  }
+
+
 
 
   return (
@@ -12,22 +29,22 @@ export default function RecipeDetail(props) {
       <h3>Recipe Detail Page</h3>
   
       <div className="img-container">
-        <img className="recipe-img" src={`/recieps/:id/${props.img_url}`} />
+        <img className="recipe-img" src={`/recieps/:id/${recipeItem?.img_url}`} />
       </div>
       <div className="recipe-content">
         <div className="recipe-title">
-          <h3>{props.name}</h3>
+          <h3>{recipeItem?.name}</h3>
         </div>
         <div className="reciep-info">
-          <p>Prep Time: {`recipes.${props.prep_time}`}</p>
-          <p>Baking/Cooking Time: {props.baking_cooking_time}</p>
-          <p>Servings: {props.servings}</p>
+          <p>Prep Time: {`recipes.${recipeItem?.prep_time}`}</p>
+          <p>Baking/Cooking Time: {recipeItem?.baking_cooking_time}</p>
+          <p>Servings: {recipeItem?.servings}</p>
         </div>
         <div className="recipe-instructions">
-          <p>Instructions: </p>
+          <p>Ingredients: {recipeItem?.ingredients} </p>
         </div>
         <div className="recipe-directions">
-          <p>Directions: </p>
+          <p>Directions: {recipeItem?.directions} </p>
         </div>
       </div>
 

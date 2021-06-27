a160500285fb9efe5a11a5653163ab04ce5a0899
Carri-anne Hamer <chamer79@gmail.com>
2021-06-27 12:24:28 -0400

    mapped out instructions and directions to render as a list

--- client/src/screens/RecipeDetail/RecipeDetail.jsx
+++ client/src/screens/RecipeDetail/RecipeDetail.jsx
@@ -8,7 +8,6 @@ export default function RecipeDetail(props) {
   const [isLoaded, setLoaded] = useState(false);
   const { id } = useParams();
   const { currentUser, handleDelete } = props;
- 
 
   useEffect(() => {
     const fetchRecipe = async () => {
@@ -25,43 +24,55 @@ export default function RecipeDetail(props) {
 
   return (
     <main>
-          <div className="put-delete-buttons">
+      <div className="put-delete-buttons">
         {currentUser ? (
-        <>
-        <Link to={`/recipes/${recipeItem?.id}/update`}>
-                   <button className="edit-button">UPDATE</button>
-                </Link>
-                <button
-                  className="delete-button"
-                  onClick={() => handleDelete(recipeItem?.id)}
-                >
-                  DELETE
-                </button>
+          <>
+            <Link to={`/recipes/${recipeItem?.id}/update`}>
+              <button className="edit-button">UPDATE</button>
+            </Link>
+            <button
+              className="delete-button"
+              onClick={() => handleDelete(recipeItem?.id)}
+            >
+              DELETE
+            </button>
           </>
-        ) : ( <></>)
-        }
-          </div>
+        ) : (
+          <></>
+        )}
+      </div>
       <section className="receipe-card-container">
         <div className="img-container">
-          <img
-            className="recipe-img"
-            src={`${recipeItem?.img_url}`}
-          />
+          <img className="recipe-img" src={`${recipeItem?.img_url}`} />
         </div>
         <div className="recipe-content">
           <div className="recipe-title">
             <h3>{recipeItem?.name}</h3>
           </div>
           <div className="reciep-info">
-            <p>Prep Time: {`recipes.${recipeItem?.prep_time}`}</p>
-            <p>Baking/Cooking Time: {recipeItem?.baking_cooking_time}</p>
-            <p>Servings: {recipeItem?.servings}</p>
+            <p><span clasName="recipe-props">Prep Time:</span> {`recipes.${recipeItem?.prep_time}`}</p>
+            <p><span clasName="recipe-props">Baking/Cooking Time:</span> {recipeItem?.baking_cooking_time}</p>
+            <p><span clasName="recipe-props">Servings:</span> {recipeItem?.servings}</p>
           </div>
-          <div className="recipe-instructions">
-            <p>Ingredients: {recipeItem?.ingredients} </p>
+          <div className="recipe-ingredients">
+            <ul className="ingredient-list">
+            <span clasName="recipe-props">Ingredients:</span>
+              {recipeItem?.ingredients.split("\n").map((ingredient) => (
+                <li className="ingredient-item" key={recipeItem?.ingredient}>
+                  {ingredient}
+                </li>
+              ))}
+            </ul>
           </div>
           <div className="recipe-directions">
-            <p>Directions: {recipeItem?.directions} </p>
+            <ul className="direction-list">
+            <span clasName="recipe-props">Directions:</span>
+              {recipeItem?.directions.split("\n").map((direction) => (
+                <li className="direction-item" key={recipeItem?.direction}>
+                  {direction}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </section>

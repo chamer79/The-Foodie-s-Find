adc906ca1100461ba58af231497f7efc69054db8
Carri-anne Hamer <chamer79@gmail.com>
2021-06-29 01:11:08 -0400

    push before Netlify deploy

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -3,33 +3,30 @@ import { NavLink } from "react-router-dom";
 export default function Layout(props) {
   const { currentUser, handleLogout, children } = props;
   return (
-    <div className="background-img">
+    <div>
       <header>
         {currentUser ? (
           <nav className="user-logged-in">
             <NavLink to="/">
-              <img
-                src="http://i.imgur.com/aHMHpXc.png"
-              />
+              <img src="http://i.imgur.com/aHMHpXc.png" />
             </NavLink>
             <p>Welcome {currentUser.username}</p>
             <NavLink to="/post-recipe">New Post</NavLink>
-            <button className="logout" onClick={handleLogout}>Logout</button>
+            <button className="logout" onClick={handleLogout}>
+              Logout
+            </button>
           </nav>
         ) : (
-            <nav className="user-logged-out">
-              <NavLink to="/">
-              <img
-                src="http://i.imgur.com/aHMHpXc.png"
-              />
+          <nav className="user-logged-out">
+            <NavLink to="/">
+              <img src="http://i.imgur.com/aHMHpXc.png" />
             </NavLink>
             <NavLink to="/login">Login</NavLink>
             <NavLink to="/signup">Sign up</NavLink>
-            </nav>
+          </nav>
         )}
-       
       </header>
       {children}
     </div>
-  )
+  );
 }

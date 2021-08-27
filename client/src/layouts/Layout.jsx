cec1368ec70c5cab1f4fa49dfbd1fae423fe47d2
Carri-anne Hamer <chamer79@gmail.com>
2021-08-27 08:25:04 -0400

    changed path for nav logo

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -7,7 +7,7 @@ export default function Layout(props) {
       <header>
         {currentUser ? (
           <nav className="user-logged-in">
-            <NavLink to="/">
+            <NavLink to="/recipes">
               <img src="http://i.imgur.com/aHMHpXc.png" />
             </NavLink>
             <p>Welcome {currentUser.username}</p>
@@ -18,7 +18,7 @@ export default function Layout(props) {
           </nav>
         ) : (
           <nav className="user-logged-out">
-            <NavLink to="/">
+            <NavLink to="/recipes">
               <img src="http://i.imgur.com/aHMHpXc.png" />
             </NavLink>
             <NavLink className="login-button" to="/login">Login</NavLink>
